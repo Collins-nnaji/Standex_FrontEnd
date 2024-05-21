8e7efefd345ab69d701585fb181a6fd14be2da6e
Collins nnaji <collinsnnaji1@gmail.com>
2024-05-21 23:59:02 +0100

    jbbbm

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,12 +5,19 @@ import './navigation.css';
 const Navigation = () => {
   return (
     <nav className="navigation">
-      <ul>
+      <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/dojo">Dojo</Link></li>
         <li><Link to="/resume-builder">Resume Builder</Link></li>
         <li><Link to="/services">Services</Link></li>
         <li><Link to="/job-board">Job Board</Link></li>
+      </ul>
+      <ul className="breadcrumb-nav">
+        <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+        <li className="breadcrumb-item"><Link to="/dojo">Dojo</Link></li>
+        <li className="breadcrumb-item"><Link to="/resume-builder">Resume Builder</Link></li>
+        <li className="breadcrumb-item"><Link to="/services">Services</Link></li>
+        <li className="breadcrumb-item"><Link to="/job-board">Job Board</Link></li>
       </ul>
     </nav>
   );
