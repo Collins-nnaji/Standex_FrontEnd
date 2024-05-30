ad91b4a69a233946e8bed465e5b7d47d8ff917a1
Collins nnaji <collinsnnaji1@gmail.com>
2024-05-31 00:53:44 +0100

    Test

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Accordion.css';
+import './accordion.css';
 
 const Accordion = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
