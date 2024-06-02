a0939871561f7be995526dc40443c8f726d8bc3f
Collins nnaji <collinsnnaji1@gmail.com>
2024-06-02 20:13:05 +0100

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
       <div className="footer-content">
         <p>&copy; 2024 Standex Digital. All rights reserved.</p>
         <div className="social-media">
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://www.instagram.com/standexdigital/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://x.com/standexdigital" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://www.linkedin.com/company/standex-digital/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </div>
       </div>
     </footer>
