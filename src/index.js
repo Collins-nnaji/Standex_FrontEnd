3efc1c090c651237f73cefdb97e0adb6f9fee0ff
Collins nnaji <collinsnnaji1@gmail.com>
2024-07-14 18:06:07 +0100

    hhhh

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const GTAG_MANAGER_ID = 'G-LLNKZEJCBP';
+
+// Function to dynamically insert the Google Tag Manager script
+const insertGtagScript = () => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GTAG_MANAGER_ID}`;
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { window.dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', GTAG_MANAGER_ID);
+  };
+  document.head.appendChild(script);
+};
+
+insertGtagScript();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
